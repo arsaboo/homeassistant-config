c44ddc0879de6964a910ed759b305ecb21f730bd
Alok <arsaboo@gmx.com>
2017-08-12 21:47:48 -0400

    Updated based on @amelchio's inputs.

--- custom_components/alarm_control_panel/abode.py
+++ custom_components/alarm_control_panel/abode.py
@@ -4,12 +4,14 @@ Interfaces with Abode Home Security System.
 """
 import os
 import logging
+import abodepy
 
 import voluptuous as vol
 
 import homeassistant.components.alarm_control_panel as alarm
+from homeassistant.util import Throttle
 from homeassistant.components.alarm_control_panel import PLATFORM_SCHEMA
-from homeassistant.const import (
+from homeassistant.const import (CONF_SCAN_INTERVAL,
     CONF_PASSWORD, CONF_USERNAME, STATE_UNKNOWN, CONF_NAME)
 import homeassistant.helpers.config_validation as cv
 import homeassistant.loader as loader
@@ -23,27 +25,27 @@ DOMAIN = 'abode'
 ALARM_STATE_HOME = 'home'
 ALARM_STATE_STANDBY = 'standby'
 ALARM_STATE_AWAY = 'away'
+DEFAULT_SCAN_INTERVAL = timedelta(seconds=600)
 
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_PASSWORD): cv.string,
     vol.Required(CONF_USERNAME): cv.string,
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+    vol.Optional(CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL): cv.positive_int
 })
+
+def abode(uname, passwd):
+    return abodepy.Abode(username=uname, password=passwd, get_devices=True)
 
 def get_abode_mode(uname, passwd):
     """Get Abode mode."""
-    abode_mode_command = 'python3 /home/arsaboo/abodepy/abodecl.py --username ' + '{}'.format(uname) + ' --password ' + '{}'.format(passwd) + ' --mode'
-    res = os.popen(abode_mode_command).readline()
-    mode = res.split(" ")
-    return mode[1].strip()
+    return abode(uname, passwd).get_alarm().mode
 
 def set_abode_mode(uname, passwd, mode):
     """Set Abode mode."""
-    abode_mode_command = 'python3 /home/arsaboo/abodepy/abodecl.py --username ' + '{}'.format(uname) + ' --password ' + '{}'.format(passwd) + ' --arm ' + '{}'.format(mode)
-    res = os.popen(abode_mode_command).readline()
-    mode = res.split(" ")
-    return mode[3].strip()
+    abode(uname, passwd).get_alarm().set_mode(mode)
+    _LOGGER.info("Abode mode changed to %s", mode)
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up the Abode platform."""
@@ -78,6 +80,7 @@ class AbodeAlarm(alarm.AlarmControlPanel):
         """Return the state of the device."""
         return self._state
 
+    @Throttle(CONF_SCAN_INTERVAL)
     def update(self):
         """Return the state of the device."""
         status = get_abode_mode(self._username, self._password)
@@ -90,22 +93,17 @@ class AbodeAlarm(alarm.AlarmControlPanel):
             state = ALARM_STATE_AWAY
         else:
             state = STATE_UNKNOWN
-
         self._state = state
-
 
 
     def alarm_disarm(self, code=None):
         """Send disarm command."""
-        status = set_abode_mode(self._username, self._password, ALARM_STATE_STANDBY)
-        _LOGGER.info("Abode mode changed to %s", status)
+        set_abode_mode(self._username, self._password, ALARM_STATE_STANDBY)
 
     def alarm_arm_home(self, code=None):
         """Send arm home command."""
-        status = set_abode_mode(self._username, self._password, ALARM_STATE_HOME)
-        _LOGGER.info("Abode mode changed to %s", status)
+        set_abode_mode(self._username, self._password, ALARM_STATE_HOME)
 
     def alarm_arm_away(self, code=None):
         """Send arm away command."""
-        status = set_abode_mode(self._username, self._password, ALARM_STATE_AWAY)
-        _LOGGER.info("Abode mode changed to %s", status)
+        set_abode_mode(self._username, self._password, ALARM_STATE_AWAY)
