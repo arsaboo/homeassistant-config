fdd287a2880ce42b5bb067c83320a427e9720e1f
Alok <arsaboo@gmx.com>
2017-10-22 14:25:40 -0400

    Added Ring video Downloader

---
+++ python_scripts/ring_download.py
@@ -0,0 +1,14 @@
+# obtain ring doorbell camera object
+# replace the camera.front_door by your camera entity
+ring_cam = hass.states.get('camera.front_door')
+
+subdir_name = 'ring_{}'.format(ring_cam.attributes.get('friendly_name'))
+
+# get video URL
+data = {
+    'url': ring_cam.attributes.get('video_url'),
+    'subdir': subdir_name,
+}
+
+# call downloader component to save the video
+hass.services.call('downloader', 'download_file', data)
