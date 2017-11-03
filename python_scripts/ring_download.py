852bca2ba09123fb2d69fe79604609214d34101d
Alok <arsaboo@gmx.com>
2017-11-03 15:34:34 -0400

    Updated Ring download python_script to use lower case

--- python_scripts/ring_download.py
+++ python_scripts/ring_download.py
@@ -2,14 +2,14 @@
 # replace the camera.front_door by your camera entity
 ring_cam = hass.states.get('camera.front_door')
 
-subdir_name = 'ring_{}'.format(ring_cam.attributes.get('friendly_name'))
+subdir_name = 'ring_{}'.format(ring_cam.attributes.get('friendly_name').lower().replace(' ', '_'))
 
 # get video URL
 data = {
     'url': ring_cam.attributes.get('video_url'),
     'subdir': subdir_name,
     'overwrite': True,
-    'filename': ring_cam.attributes.get('friendly_name')
+    'filename': ring_cam.attributes.get('friendly_name').lower().replace(' ', '_')
 }
 
 # call downloader component to save the video
