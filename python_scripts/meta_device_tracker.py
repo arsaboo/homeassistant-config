32b75a67d33fe50fe02b0607a12d719e62cc95b3
Alok <arsaboo@gmx.com>
2018-04-29 13:00:41 -0400

    Changed iPhones

--- python_scripts/meta_device_tracker.py
+++ python_scripts/meta_device_tracker.py
@@ -8,10 +8,10 @@
 # List the trackers for each individual
 RashmiTrackers = ['device_tracker.rashmisiphone', 'device_tracker.pi_rashmiphone',
                   'device_tracker.rashmiappiphone', 'device_tracker.sonu_sonu',
-                  'device_tracker.3a196aa7e85e4ab5bee4e3d3af667a09']
+                  'device_tracker.b4445761f6c04b7f835fcfdc03b47111']
 AlokTrackers = ['device_tracker.myiphone', 'device_tracker.pi_alokphone',
                 'device_tracker.alokiosiphone', 'device_tracker.alok_alok',
-                'device_tracker.elantrase', 'device_tracker.6b00226793104872afb9248f8a45f957']
+                'device_tracker.elantrase', 'device_tracker.b4445761f6c04b7f835fcfdc03b47111']
 # Get the entity that triggered the automation
 triggeredEntity = data.get('entity_id')
 
