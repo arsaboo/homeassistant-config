4b41c317604cc6d1a429a4cc3b152294d4cc9e1b
Alok <arsaboo@gmx.com>
2018-04-29 13:01:03 -0400

    Updated facebox

--- custom_components/image_processing/facebox.py
+++ custom_components/image_processing/facebox.py
@@ -14,7 +14,9 @@ from homeassistant.core import split_entity_id
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.image_processing import (
     PLATFORM_SCHEMA, CONF_SOURCE, CONF_ENTITY_ID,
-    CONF_NAME, ImageProcessingEntity)
+    CONF_NAME)
+from homeassistant.components.image_processing.microsoft_face_identify import (
+    ImageProcessingFaceEntity)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -33,12 +35,12 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
         entities.append(Facebox(
             camera.get(CONF_NAME),
             config[CONF_ENDPOINT],
-            camera[CONF_ENTITY_ID],
+            camera[CONF_ENTITY_ID]
         ))
     add_devices(entities)
 
 
-class Facebox(ImageProcessingEntity):
+class Facebox(ImageProcessingFaceEntity):
     """Perform a classification via a Facebox."""
 
     def __init__(self, name, endpoint, camera_entity):
@@ -49,40 +51,41 @@ class Facebox(ImageProcessingEntity):
         else:
             self._name = "Facebox {0}".format(
                 split_entity_id(camera_entity)[1])
-        self._camera = camera_entity
         self._url = "http://{}/facebox/check".format(endpoint)
-        self._state = "No_processing_performed"
-        self._faces = {}
+        self._camera = camera_entity
         self._response_time = None
+        self.total_faces = 0
+        self.faces = []
 
     def process_image(self, image):
         """Process an image."""
         timer_start = time.perf_counter()
-        response = requests.post(
-            self._url,
-            json=self.encode_image(image)
-            ).json()
+        response = {}
+        try:
+            response = requests.post(
+                self._url,
+                json=self.encode_image(image),
+                timeout=30
+                ).json()
+        except requests.exceptions.ConnectionError:
+            _LOGGER.error("ConnectionError: Is Facebox running?")
+            response['success'] = False
 
         if response['success']:
             elapsed_time = time.perf_counter() - timer_start
             self._response_time = "{} seconds".format(
                     str(round(elapsed_time, 1)))
-            self._state = int(response['facesCount'])
-            self._faces = self.process_faces(response)
+            self.total_faces = response['facesCount']  # An int.
+            self.faces = response['faces']
+
         else:
-            self._state = "Processing_not_successful"
-            self._faces = {}
+            self.total_faces = "Request_failed"
+            self.faces = []
 
     def encode_image(self, image):
         """base64 encode an image stream."""
         base64_img = base64.b64encode(image).decode('ascii')
         return {"base64": base64_img}
-
-    def process_faces(self, response):
-        """Process face data from facebox response."""
-        faces = {face['name']: round(face['confidence'], ROUNDING_DECIMALS)
-                 for face in response['faces'] if face['matched']}
-        return faces
 
     @property
     def camera_entity(self):
@@ -95,25 +98,8 @@ class Facebox(ImageProcessingEntity):
         return self._name
 
     @property
-    def state(self):
-        """Return the state of the entity."""
-        return self._state
-
-    @property
     def device_state_attributes(self):
-        """Return other details about the sensor state."""
-        attr = self._faces
-        attr.update({'response_time': self._response_time})
-        return attr
-
-    @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement of this entity, if any."""
-        if isinstance(self._state, int):
-            if self._state == 1:
-                unit_of_measurement = "face"
-            else:
-                unit_of_measurement = "faces"
-        else:
-            unit_of_measurement = None
-        return unit_of_measurement
+        """Return the other state attributes."""
+        return {
+            'response_time': self._response_time,
+        }
