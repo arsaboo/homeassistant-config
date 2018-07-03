04e9cb46f583a2d56fe1ddd294762a039bbc5492
Alok <arsaboo@gmx.com>
2018-07-03 15:44:16 -0400

    Updated to 0.73.0b5

--- www/custom_ui/weather-card.js
+++ www/custom_ui/weather-card.js
@@ -1,125 +1,114 @@
-class weatherCard extends HTMLElement {
+class WeatherCard extends HTMLElement {
   set hass(hass) {
     if (!this.content) {
       const card = document.createElement('ha-card');
-      card.header = 'Weather Forecast';
+      // card.header = 'Weather Forecast';
       const link = document.createElement('link');
       link.type = 'text/css';
       link.rel = 'stylesheet';
-      link.href = '/local/css/homeassistant.css';
+      link.href = '/local/custom_ui/weather-card.css';
       card.appendChild(link);
       this.content = document.createElement('div');
       this.content.className = 'card';
       card.appendChild(this.content);
       this.appendChild(card);
-      console.log('b',this);
     }
 
-    var currentCondition;
-    var humidity;
-    var i;
-    var iconToUse;
-    var pressure;
-    var sunLocation;
-    var temperature;
-    var visibility;
-    var windBearing;
-    var windSpeed;
+    const getUnit = function (measure) {
+      const lengthUnit = hass.config.core.unit_system.length;
+      switch (measure) {
+        case 'air_pressure':
+          return lengthUnit === 'km' ? 'hPa' : 'inHg';
+        case 'length':
+          return lengthUnit;
+        case 'precipitation':
+          return lengthUnit === 'km' ? 'mm' : 'in';
+        default:
+          return hass.config.core.unit_system[measure] || '';
+      }
+    };
 
-  var dayWeatherIcons = {
-    'clear-night': 'day',
-    'cloudy': 'cloudy',
-    'fog': 'cloudy',
-    'hail': 'rainy-7',
-    'lightning': 'thunder',
-    'lightning-rainy': 'thunder',
-    'partlycloudy': 'cloudy-day-3',
-    'pouring': 'rainy-6',
-    'rainy': 'rainy-5',
-    'snowy': 'snowy-6',
-    'snowy-rainy': 'rainy-7',
-    'sunny': 'day',
-    'windy': 'cloudy',
-    'windy-variant': 'cloudy-day-3',
-    'exceptional': '!!',
-  };
-
-  var nightWeatherIcons = {
-    'clear-night': 'night',
-    'cloudy': 'cloudy',
-    'fog': 'cloudy',
-    'hail': 'rainy-7',
-    'lightning': 'thunder',
-    'lightning-rainy': 'thunder',
-    'partlycloudy': 'cloudy-night-3',
-    'pouring': 'rainy-6',
-    'rainy': 'rainy-5',
-    'snowy': 'snowy-6',
-    'snowy-rainy': 'rainy-7',
-    'sunny': 'night',
-    'windy': 'cloudy',
-    'windy-variant': 'cloudy-night-3',
-    'exceptional': '!!',
-  };
-
-  var windDirections = [
-    'N',
-    'NNE',
-    'NE',
-    'ENE',
-    'E',
-    'ESE',
-    'SE',
-    'SSE',
-    'S',
-    'SSW',
-    'SW',
-    'WSW',
-    'W',
-    'WNW',
-    'NW',
-    'NNW',
-    'N'
-  ];
-    //console.log('a',hass);
-    for (i=0; i < this.config.entities.length; i++) {
-        if (hass.states[this.config.entities[i]]._domain == "sun") {
-            sunLocation = hass.states[this.config.entities[i]].state;
-        } else if (hass.states[this.config.entities[i]]._domain == "weather") {
-            currentCondition = hass.states[this.config.entities[i]].state;
-            humidity = hass.states[this.config.entities[i]].attributes.humidity;
-            pressure = hass.states[this.config.entities[i]].attributes.pressure;
-            temperature = Math.round(hass.states[this.config.entities[i]].attributes.temperature);
-            visibility = hass.states[this.config.entities[i]].attributes.visibility;
-            windBearing = windDirections[(parseInt((hass.states[this.config.entities[i]].attributes.wind_bearing + 11.25) / 22.5))];
-            windSpeed = hass.states[this.config.entities[i]].attributes.wind_speed;
-        }
+    const transformDayNight = {
+      "below_horizon": "night",
+      "above_horizon": "day",
+    }
+    const sunLocation = transformDayNight[hass.states[this.config.entity_sun].state];
+    const weatherIcons = {
+      'clear-night': `${sunLocation}`,
+      'cloudy': 'cloudy',
+      'fog': 'cloudy',
+      'hail': 'rainy-7',
+      'lightning': 'thunder',
+      'lightning-rainy': 'thunder',
+      'partlycloudy': `cloudy-${sunLocation}-3`,
+      'pouring': 'rainy-6',
+      'rainy': 'rainy-5',
+      'snowy': 'snowy-6',
+      'snowy-rainy': 'rainy-7',
+      'sunny': `${sunLocation}`,
+      'windy': 'cloudy',
+      'windy-variant': `cloudy-${sunLocation}-3`,
+      'exceptional': '!!',
     }
 
-    if (sunLocation == 'above_horizon') {
-      iconToUse = dayWeatherIcons[currentCondition];
-    } if (sunLocation == 'below_horizon') {
-      iconToUse = nightWeatherIcons[currentCondition];
-    }
+    const windDirections = [
+      'N',
+      'NNE',
+      'NE',
+      'ENE',
+      'E',
+      'ESE',
+      'SE',
+      'SSE',
+      'S',
+      'SSW',
+      'SW',
+      'WSW',
+      'W',
+      'WNW',
+      'NW',
+      'NNW',
+      'N'
+    ];
+    const entity = hass.states[this.config.entity_weather];
+    const currentCondition = entity.state;
+    const humidity = entity.attributes.humidity;
+    const pressure = entity.attributes.pressure;
+    const temperature = Math.round(entity.attributes.temperature);
+    const visibility = entity.attributes.visibility;
+    const windBearing = windDirections[(parseInt((entity.attributes.wind_bearing + 11.25) / 22.5))];
+    const windSpeed = entity.attributes.wind_speed;
+    const forecast = entity.attributes.forecast.slice(0, 5);
 
-    if (iconToUse) {
-        //this.content = document.createElement('span');
-      this.content.innerHTML = `
-      <span class="icon bigger" style="background: none, url(/local/icons/weather_icons/animated/` + iconToUse + `.svg) no-repeat; background-size: contain;">` + currentCondition + `</span>
-      <span class="temp">` + temperature + `</span><span class="temp_unit">°F</span>
-      <br>
-      <span>
+    this.content.innerHTML = `
+      <span class="icon bigger" style="background: none, url(/local/icons/weather_icons/animated/${weatherIcons[currentCondition]}.svg) no-repeat; background-size: contain;">${currentCondition}</span>
+      <span class="temp">${temperature}</span><span class="tempc"> ${getUnit('temperature')}</span>
+      <div>
         <ul class="variations right">
-            <li><span class="iron-icon"><iron-icon icon="mdi:water-percent"></iron-icon></span>` + humidity + `<span class="unit"> %</span></li>
-            <li><span class="iron-icon"><iron-icon icon="mdi:gauge"></iron-icon></span>` + pressure + `<span class="unit"> hPa</span></li>
+            <li><span class="ha-icon"><ha-icon icon="mdi:water-percent"></ha-icon></span>${humidity}<span class="unit"> %</span></li>
+            <li><span class="ha-icon"><ha-icon icon="mdi:gauge"></ha-icon></span>${pressure}<span class="unit"> ${getUnit('temperature')}</span></li>
         </ul>
         <ul class="variations">
-            <li><span class="iron-icon"><iron-icon icon="mdi:weather-windy"></iron-icon></span>` + windBearing + ` ` + windSpeed + `<span class="unit"> mi/h</span></li>
-            <li><span class="iron-icon"><iron-icon icon="mdi:weather-fog"></iron-icon></span>` + visibility + `<span class="unit"> mi</span></li>
+            <li><span class="ha-icon"><ha-icon icon="mdi:weather-windy"></ha-icon></span>${windBearing} ${windSpeed}<span class="unit"> ${getUnit('length')}/h</span></li>
+            <li><span class="ha-icon"><ha-icon icon="mdi:weather-fog"></ha-icon></span>${visibility}<span class="unit"> ${getUnit('length')}</span></li>
         </ul>
-      </span>
-        `;
+      </div>
+      <div class="forecast clear">
+          ${forecast.map(daily => `
+              <div class="day">
+                  <span class="dayname">${(new Date(daily.datetime)).toString().split(' ')[0]}</span>
+                  <br><i class="icon" style="background: none, url(/local/icons/weather_icons/animated/${weatherIcons[daily.condition]}.svg) no-repeat; background-size: contain;"></i>
+                  <br><span class="highTemp">${daily.temperature}${getUnit('temperature')}</span>
+                  <br><span class="lowTemp">${daily.templow}${getUnit('temperature')}</span>
+              </div>`).join('')}
+      </div>`;
+  }
+
+  setConfig(config) {
+    if (!config.entity_weather || !config.entity_sun) {
+      throw new Error('Please define entities');
     }
+    this.config = config;
   }
 
   // The height of your card. Home Assistant uses this to automatically
@@ -129,4 +118,4 @@ class weatherCard extends HTMLElement {
   }
 }
 
-customElements.define('weather-card', weatherCard);
+customElements.define('weather-card', WeatherCard);
