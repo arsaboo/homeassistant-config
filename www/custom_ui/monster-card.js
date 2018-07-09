15d798e859308efad0b5feedb435e09359b2e238
Alok <arsaboo@gmx.com>
2018-07-09 13:30:07 -0400

    Updated custom lovelace cards

--- www/custom_ui/monster-card.js
+++ www/custom_ui/monster-card.js
@@ -1,40 +1,40 @@
-function _filterEntityId(stateObj, pattern) {
-  if (pattern.indexOf('*') === -1) {
-    return stateObj.entity_id === pattern;
-  }
-  const regEx = new RegExp(`^${pattern.replace(/\*/g, '.*')}$`);
-  return stateObj.entity_id.search(regEx) === 0;
-}
-
-function _getEntities(hass, filters) {
-  const entities = new Set();
-  filters.forEach((filter) => {
-    const filters = [];
-    if (filter.domain) {
-      filters.push(stateObj => stateObj.entity_id.split('.', 1)[0] === filter.domain);
-    }
-    if (filter.attributes) {
-      Object.keys(filter.attributes).forEach(key => {
-        filters.push(stateObj => stateObj.attributes[key] === filter.attributes[key]);
-      });
-    }
-    if (filter.entity_id) {
-      filters.push(stateObj => _filterEntityId(stateObj, filter.entity_id));
-    }
-    if (filter.state) {
-      filters.push(stateObj => stateObj.state === filter.state);
-    }
-
-    Object.values(hass.states).forEach((stateObj) => {
-      if (filters.every(filterFunc => filterFunc(stateObj))) {
-        entities.add(stateObj.entity_id);
-      }
-    });
-  });
-  return Array.from(entities);
-}
-
 class MonsterCard extends HTMLElement {
+
+  _getEntities(hass, filters) {
+    function _filterEntityId(stateObj, pattern) {
+      if (pattern.indexOf('*') === -1) {
+        return stateObj.entity_id === pattern;
+      }
+      const regEx = new RegExp(`^${pattern.replace(/\*/g, '.*')}$`);
+      return stateObj.entity_id.search(regEx) === 0;
+    }
+    const entities = new Set();
+    filters.forEach((filter) => {
+      const filters = [];
+      if (filter.domain) {
+        filters.push(stateObj => stateObj.entity_id.split('.', 1)[0] === filter.domain);
+      }
+      if (filter.attributes) {
+        Object.keys(filter.attributes).forEach(key => {
+          filters.push(stateObj => stateObj.attributes[key] === filter.attributes[key]);
+        });
+      }
+      if (filter.entity_id) {
+        filters.push(stateObj => _filterEntityId(stateObj, filter.entity_id));
+      }
+      if (filter.state) {
+        filters.push(stateObj => stateObj.state === filter.state);
+      }
+
+      Object.values(hass.states).forEach((stateObj) => {
+        if (filters.every(filterFunc => filterFunc(stateObj))) {
+          entities.add(stateObj.entity_id);
+        }
+      });
+    });
+    return Array.from(entities);
+  }
+
   setConfig(config) {
     if (!config.filter.include || !Array.isArray(config.filter.include)) {
       throw new Error('Please define filters');
@@ -44,6 +44,7 @@ class MonsterCard extends HTMLElement {
 
     const cardConfig = Object.assign({}, config);
     if (!cardConfig.card) cardConfig.card = {};
+    if (config.card.entities) delete config.card.entities;
     if (!cardConfig.card.type) cardConfig.card.type = 'entities';
 
     const element = document.createElement(`hui-${cardConfig.card.type}-card`);
@@ -54,18 +55,30 @@ class MonsterCard extends HTMLElement {
 
   set hass(hass) {
     const config = this._config;
-    let entities = _getEntities(hass, config.filter.include);
+    let entities = this._getEntities(hass, config.filter.include);
     if (config.filter.exclude) {
-      const excludeEntities = _getEntities(hass, config.filter.exclude);
+      const excludeEntities = this._getEntities(hass, config.filter.exclude);
       entities = entities.filter(entity => !excludeEntities.includes(entity));
     }
-    entities.sort();
+
+
+
+    if (entities.length === 0 && config.show_empty === false) {
+      this.style.display = 'none';
+    } else {
+      if (config.when && (hass.states[config.when.entity].state == config.when.state) || !config.when) {
+        this.style.display = 'block';
+        entities.sort();
+      } else {
+        this.style.display = 'none';
+      }
+    }
 
     if (!config.card.entities || config.card.entities.length !== entities.length ||
-        !config.card.entities.every((value, index) => value === entities[index])) {
+      !config.card.entities.every((value, index) => value === entities[index])) {
       config.card.entities = entities;
+      this.lastChild.setConfig(config.card);
     }
-    this.lastChild.setConfig(config.card);
     this.lastChild.hass = hass;
   }
 
