aff77d20f007f17c01d107e5cd0a9f94321813f7
Alok <arsaboo@gmx.com>
2018-08-28 11:03:50 -0400

    Updated personal capital sensor

--- custom_components/sensor/personalcapital.py
+++ custom_components/sensor/personalcapital.py
@@ -21,6 +21,7 @@ CONF_EMAIL = 'email'
 CONF_PASSWORD = 'password'
 CONF_UNIT_OF_MEASUREMENT = 'unit_of_measurement'
 
+SESSION_FILE = '.pc-session'
 DATA_PERSONAL_CAPITAL = 'personalcapital_cache'
 
 ATTR_NETWORTH = 'networth'
@@ -59,6 +60,7 @@ def request_app_setup(hass, config, pc, add_devices, discovery_info=None):
         if result == RequireTwoFactorException:
             configurator.notify_errors(_CONFIGURING['personalcapital'], "Invalid verification code")
         else:
+            save_session(hass, pc.get_session())
             continue_setup_platform(hass, config, pc, add_devices, discovery_info)
 
     if 'personalcapital' not in _CONFIGURING:
@@ -78,27 +80,55 @@ def request_app_setup(hass, config, pc, add_devices, discovery_info=None):
             'type': 'string'}]
     )
 
+def load_session(hass):
+    try:
+        with open(hass.config.path(SESSION_FILE)) as data_file:    
+            cookies = {}
+            try:
+                cookies = json.load(data_file)
+            except ValueError as err:
+                return {}
+            return cookies
+    except IOError as err:
+        return {}
+
+def save_session(hass, session):
+    with open(hass.config.path(SESSION_FILE), 'w') as data_file:
+        data_file.write(json.dumps(session))
+
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up the Personal Capital component."""
     from personalcapital import PersonalCapital, RequireTwoFactorException, TwoFactorVerificationModeEnum
     pc = PersonalCapital()
-    request_app_setup(hass, config, pc, add_devices, discovery_info)
+    session = load_session(hass)
+
+    if len(session) > 0:
+        pc.set_session(session)
+
+        try:
+            pc.login(config.get(CONF_EMAIL), config.get(CONF_PASSWORD))
+            continue_setup_platform(hass, config, pc, add_devices, discovery_info)
+        except RequireTwoFactorException:
+            request_app_setup(hass, config, pc, add_devices, discovery_info)
+    else:
+        request_app_setup(hass, config, pc, add_devices, discovery_info)
 
 def continue_setup_platform(hass, config, pc, add_devices, discovery_info=None):
     """Set up the Personal Capital component."""
     if "personalcapital" in _CONFIGURING:
         hass.components.configurator.request_done(_CONFIGURING.pop("personalcapital"))
-        uom = config.get(CONF_UNIT_OF_MEASUREMENT)
-        add_devices([PersonalCapitalNetWorthSensor(pc, uom)], True)
-        add_devices([PersonalCapitalCategorySensor(hass, pc, uom, 'BANK', '', 'assets', 'Assets')], True)
-        add_devices([PersonalCapitalCategorySensor(hass, pc, uom, 'LIABILITIES', '', 'liabilities', 'Liabilities')], True)
-        add_devices([PersonalCapitalCategorySensor(hass, pc, uom, 'INVESTMENT', '', 'investmentAccountsTotal', 'Investments')], True)
-        add_devices([PersonalCapitalCategorySensor(hass, pc, uom, 'MORTGAGE', '', 'mortgageAccountsTotal', 'Mortgages')], True)
-        add_devices([PersonalCapitalCategorySensor(hass, pc, uom, 'BANK', 'Cash', 'cashAccountsTotal', 'Cash')], True)
-        add_devices([PersonalCapitalCategorySensor(hass, pc, uom, 'OTHER_ASSETS', '', 'otherAssetAccountsTotal', 'Other Assets')], True)
-        add_devices([PersonalCapitalCategorySensor(hass, pc, uom, 'OTHER_LIABILITIES', '', 'otherLiabilitiesAccountsTotal', 'Other Liabilities')], True)
-        add_devices([PersonalCapitalCategorySensor(hass, pc, uom, 'CREDIT_CARD', '', 'creditCardAccountsTotal', 'Credit')], True)
-        add_devices([PersonalCapitalCategorySensor(hass, pc, uom, 'LOAN', '', 'loanAccountsTotal', 'Loans')], True)
+
+    uom = config.get(CONF_UNIT_OF_MEASUREMENT)
+    add_devices([PersonalCapitalNetWorthSensor(pc, uom)], True)
+    add_devices([PersonalCapitalCategorySensor(hass, pc, uom, 'BANK', '', 'assets', 'Assets')], True)
+    add_devices([PersonalCapitalCategorySensor(hass, pc, uom, 'LIABILITIES', '', 'liabilities', 'Liabilities')], True)
+    add_devices([PersonalCapitalCategorySensor(hass, pc, uom, 'INVESTMENT', '', 'investmentAccountsTotal', 'Investments')], True)
+    add_devices([PersonalCapitalCategorySensor(hass, pc, uom, 'MORTGAGE', '', 'mortgageAccountsTotal', 'Mortgages')], True)
+    add_devices([PersonalCapitalCategorySensor(hass, pc, uom, 'BANK', 'Cash', 'cashAccountsTotal', 'Cash')], True)
+    add_devices([PersonalCapitalCategorySensor(hass, pc, uom, 'OTHER_ASSETS', '', 'otherAssetAccountsTotal', 'Other Assets')], True)
+    add_devices([PersonalCapitalCategorySensor(hass, pc, uom, 'OTHER_LIABILITIES', '', 'otherLiabilitiesAccountsTotal', 'Other Liabilities')], True)
+    add_devices([PersonalCapitalCategorySensor(hass, pc, uom, 'CREDIT_CARD', '', 'creditCardAccountsTotal', 'Credit')], True)
+    add_devices([PersonalCapitalCategorySensor(hass, pc, uom, 'LOAN', '', 'loanAccountsTotal', 'Loans')], True)
 
 class PersonalCapitalNetWorthSensor(Entity):
     """Representation of a personalcapital.com net worth sensor."""
@@ -128,8 +158,6 @@ class PersonalCapitalNetWorthSensor(Entity):
             return False
 
         spData = result.json()['spData']
-        # TODO If no spData, need to reauthenticate
-        # _LOGGER.warn(spData)
         self._state = spData.get('networth', 0.0)
         self._networth = spData.get('networth', 0.0)
         self._assets = spData.get('assets', 0.0)
@@ -201,10 +229,8 @@ class PersonalCapitalCategorySensor(Entity):
             return False
 
         spData = result.json()['spData']
-        # TODO If no spData, need to reauthenticate
         self._state = spData.get(self._balanceName, 0.0)
         accounts = spData.get('accounts')
-        # _LOGGER.warn(spData)
 
         for account in accounts:
             if self._productType == account.get('productType') and \
