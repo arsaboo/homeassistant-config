79bd15f93b58cde0716f489abfa69e426e8c271b
Alok <arsaboo@gmx.com>
2018-11-02 14:35:16 -0400

    Updated TF component

--- custom_components/image_processing/tensorflow.py
+++ custom_components/image_processing/tensorflow.py
@@ -12,17 +12,19 @@ import logging
 import voluptuous as vol
 
 from homeassistant.components.image_processing import (
-    CONF_ENTITY_ID, CONF_NAME, CONF_SOURCE, CONF_CONFIDENCE, PLATFORM_SCHEMA,
+    CONF_CONFIDENCE, CONF_ENTITY_ID, CONF_NAME, CONF_SOURCE, PLATFORM_SCHEMA,
     ImageProcessingEntity)
 from homeassistant.core import split_entity_id
-import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers import template
+import homeassistant.helpers.config_validation as cv
 
-REQUIREMENTS = ['numpy==1.15.2', 'pillow==5.2.0', 'protobuf==3.6.1']
+REQUIREMENTS = ['numpy==1.15.3', 'pillow==5.2.0',
+                'protobuf==3.6.1', 'tensorflow==1.11.0']
 
 _LOGGER = logging.getLogger(__name__)
 
 ATTR_MATCHES = 'matches'
+ATTR_SUMMARY = 'summary'
 ATTR_TOTAL_MATCHES = 'total_matches'
 
 CONF_FILE_OUT = 'file_out'
@@ -37,10 +39,6 @@ CONF_TOP = 'top'
 CONF_LEFT = 'left'
 CONF_BOTTOM = 'bottom'
 CONF_RIGHT = 'right'
-
-DEFAULT_MODEL_DIR = '/usr/src/app/tensorflow'
-DEFAULT_LABELS = ("{0}/object_detection/data/mscoco_label_map.pbtxt"
-                  .format(DEFAULT_MODEL_DIR))
 
 AREA_SCHEMA = vol.Schema({
     vol.Optional(CONF_TOP, default=0): cv.small_float,
@@ -59,8 +57,8 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
         vol.All(cv.ensure_list, [cv.template]),
     vol.Required(CONF_MODEL): vol.Schema({
         vol.Required(CONF_GRAPH): cv.isfile,
-        vol.Optional(CONF_LABELS, default=DEFAULT_LABELS): cv.isfile,
-        vol.Optional(CONF_MODEL_DIR, default=DEFAULT_MODEL_DIR): cv.isdir,
+        vol.Optional(CONF_LABELS): cv.isfile,
+        vol.Optional(CONF_MODEL_DIR): cv.isdir,
         vol.Optional(CONF_AREA): AREA_SCHEMA,
         vol.Optional(CONF_CATEGORIES, default=[]):
             vol.All(cv.ensure_list, [vol.Any(
@@ -70,12 +68,23 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     })
 })
 
+
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the TensorFlow image processing platform."""
     import sys
+    import os
 
     model_config = config.get(CONF_MODEL)
-    model_dir = model_config.get(CONF_MODEL_DIR)
+    model_dir = model_config.get(CONF_MODEL_DIR) \
+        or hass.config.path('deps', 'tensorflow')
+    labels = model_config.get(CONF_LABELS) \
+        or hass.config.path('deps', 'tensorflow', 'object_detection',
+                            'data', 'mscoco_label_map.pbtxt')
+
+    # Make sure locations exist
+    if not os.path.isdir(model_dir) or not os.path.exists(labels):
+        _LOGGER.error("Unable to locate tensorflow models or label map.")
+        return
 
     # append custom model path to sys.path
     sys.path.append(model_dir)
@@ -83,14 +92,15 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     try:
         # Verify that the TensorFlow Object Detection API is pre-installed
         # pylint: disable=unused-import,unused-variable
+        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
         import tensorflow as tf # noqa
         from object_detection.utils import label_map_util # noqa
     except ImportError:
         # pylint: disable=line-too-long
         _LOGGER.error(
-            "No TensorFlow Object Detection library found! Install or compile for your system "
-            "following instructions here: "
-            "https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/installation.md")
+            "No TensorFlow Object Detection library found! Install or compile "
+            "for your system following instructions here: "
+            "https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/installation.md") # noqa
         return
 
     try:
@@ -99,9 +109,11 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
         import cv2 # noqa
     except ImportError:
         _LOGGER.warning("No OpenCV library found. "
-                        "TensorFlow will process image with PIL at reduced resolution.")
+                        "TensorFlow will process image with "
+                        "PIL at reduced resolution.")
 
     # setup tensorflow graph, session, and label map to pass to processor
+    # pylint: disable=no-member
     detection_graph = tf.Graph()
     with detection_graph.as_default():
         od_graph_def = tf.GraphDef()
@@ -111,7 +123,7 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
             tf.import_graph_def(od_graph_def, name='')
 
     session = tf.Session(graph=detection_graph)
-    label_map = label_map_util.load_labelmap(model_config.get(CONF_LABELS))
+    label_map = label_map_util.load_labelmap(labels)
     categories = label_map_util.convert_label_map_to_categories(
         label_map, max_num_classes=90, use_display_name=True)
     category_index = label_map_util.create_category_index(categories)
@@ -132,16 +144,16 @@ class TensorFlowImageProcessor(ImageProcessingEntity):
     def __init__(self, hass, camera_entity, name, session, detection_graph,
                  category_index, config):
         """Initialize the TensorFlow entity."""
-
         model_config = config.get(CONF_MODEL)
         self.hass = hass
         self._camera_entity = camera_entity
         if name:
             self._name = name
         else:
-            self._name = "TensorFlow {0}".format(split_entity_id(camera_entity)[1])
+            self._name = "TensorFlow {0}".format(
+                split_entity_id(camera_entity)[1])
         self._session = session
-        self._detection_graph = detection_graph
+        self._graph = detection_graph
         self._category_index = category_index
         self._min_confidence = config.get(CONF_CONFIDENCE)
         self._file_out = config.get(CONF_FILE_OUT)
@@ -204,17 +216,10 @@ class TensorFlowImageProcessor(ImageProcessingEntity):
         """Return device specific state attributes."""
         return {
             ATTR_MATCHES: self._matches,
+            ATTR_SUMMARY: {category: len(values)
+                           for category, values in self._matches.items()},
             ATTR_TOTAL_MATCHES: self._total_matches
         }
-
-    def _draw_box(self, draw, box, img_width, img_height, text = '', color = (255, 255, 0)):
-        ymin, xmin, ymax, xmax = box
-        (left, right, top, bottom) = (xmin * img_width, xmax * img_width,
-                                        ymin * img_height, ymax * img_height)
-        draw.line([(left, top), (left, bottom), (right, bottom),
-                    (right, top), (left, top)], width=5, fill=color)
-        if text:
-            draw.text((left, abs(top-15)), text, fill=color)
 
     def _save_image(self, image, matches, paths):
         from PIL import Image, ImageDraw
@@ -223,27 +228,38 @@ class TensorFlowImageProcessor(ImageProcessingEntity):
         img_width, img_height = img.size
         draw = ImageDraw.Draw(img)
 
+        def draw_box(draw, box, img_width,
+                     img_height, text='', color=(255, 255, 0)):
+            ymin, xmin, ymax, xmax = box
+            (left, right, top, bottom) = (xmin * img_width, xmax * img_width,
+                                          ymin * img_height, ymax * img_height)
+            draw.line([(left, top), (left, bottom), (right, bottom),
+                       (right, top), (left, top)], width=5, fill=color)
+            if text:
+                draw.text((left, abs(top-15)), text, fill=color)
+
         # Draw custom global region/area
         if self._area != [0, 0, 1, 1]:
-            self._draw_box(draw, self._area,
-                           img_width, img_height,
-                           'Detection Area', (0, 255, 255))
+            draw_box(draw, self._area,
+                     img_width, img_height,
+                     "Detection Area", (0, 255, 255))
 
         for category, values in matches.items():
             # Draw custom category regions/areas
             if self._category_areas[category] != [0, 0, 1, 1]:
-                self._draw_box(draw, self._category_areas[category], img_width, img_height,
-                               "{} Detection Area".format(category.capitalize()), (0, 255, 0))
+                label = "{} Detection Area".format(category.capitalize())
+                draw_box(draw, self._category_areas[category], img_width,
+                         img_height, label, (0, 255, 0))
 
             # Draw detected objects
             for instance in values:
-                label = "{0} {1:.1f}%".format(category, instance["score"])
-                self._draw_box(draw, instance["box"],
-                               img_width, img_height,
-                               label, (255, 255, 0))
+                label = "{0} {1:.1f}%".format(category, instance['score'])
+                draw_box(draw, instance['box'],
+                         img_width, img_height,
+                         label, (255, 255, 0))
 
         for path in paths:
-            _LOGGER.info("Saving results image to {}".format(path))
+            _LOGGER.info("Saving results image to %s", path)
             img.save(path)
 
     def process_image(self, image):
@@ -252,7 +268,8 @@ class TensorFlowImageProcessor(ImageProcessingEntity):
 
         try:
             import cv2  # pylint: disable=import-error
-            img = cv2.imdecode(np.asarray(bytearray(image)), cv2.IMREAD_UNCHANGED)
+            img = cv2.imdecode(
+                np.asarray(bytearray(image)), cv2.IMREAD_UNCHANGED)
             inp = img[:, :, [2, 1, 0]]  # BGR->RGB
             inp_expanded = inp.reshape(1, inp.shape[0], inp.shape[1], 3)
         except ImportError:
@@ -265,10 +282,10 @@ class TensorFlowImageProcessor(ImageProcessingEntity):
                 (img_height, img_width, 3)).astype(np.uint8)
             inp_expanded = np.expand_dims(inp, axis=0)
 
-        image_tensor = self._detection_graph.get_tensor_by_name('image_tensor:0')
-        boxes = self._detection_graph.get_tensor_by_name('detection_boxes:0')
-        scores = self._detection_graph.get_tensor_by_name('detection_scores:0')
-        classes = self._detection_graph.get_tensor_by_name('detection_classes:0')
+        image_tensor = self._graph.get_tensor_by_name('image_tensor:0')
+        boxes = self._graph.get_tensor_by_name('detection_boxes:0')
+        scores = self._graph.get_tensor_by_name('detection_scores:0')
+        classes = self._graph.get_tensor_by_name('detection_classes:0')
         boxes, scores, classes = self._session.run(
             [boxes, scores, classes],
             feed_dict={image_tensor: inp_expanded})
@@ -284,24 +301,25 @@ class TensorFlowImageProcessor(ImageProcessingEntity):
             # Exclude matches below min confidence value
             if score < self._min_confidence:
                 continue
-            
+
             # Exclude matches outside global area definition
             if (boxes[0] < self._area[0] or boxes[1] < self._area[1]
                     or boxes[2] > self._area[2] or boxes[3] > self._area[3]):
                 continue
-            
+
             category = self._category_index[obj_class]['name']
 
             # Exclude unlisted categories
-            if self._include_categories and category not in self._include_categories:
+            if (self._include_categories
+                    and category not in self._include_categories):
                 continue
 
             # Exclude matches outside category specific area definition
             if (self._category_areas
                     and (boxes[0] < self._category_areas[category][0]
-                    or boxes[1] < self._category_areas[category][1]
-                    or boxes[2] > self._category_areas[category][2]
-                    or boxes[3] > self._category_areas[category][3])):
+                         or boxes[1] < self._category_areas[category][1]
+                         or boxes[2] > self._category_areas[category][2]
+                         or boxes[3] > self._category_areas[category][3])):
                 continue
 
             # If we got here, we should include it
@@ -318,7 +336,8 @@ class TensorFlowImageProcessor(ImageProcessingEntity):
             paths = []
             for path_template in self._file_out:
                 if isinstance(path_template, template.Template):
-                    paths.append(path_template.render(camera_entity=self._camera_entity))
+                    paths.append(path_template.render(
+                        camera_entity=self._camera_entity))
                 else:
                     paths.append(path_template)
             self._save_image(image, matches, paths)
