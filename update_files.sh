f98106ebaf55b12c3193d161fce559f3b8e4cb21
Alok <arsaboo@gmx.com>
2019-02-14 11:00:18 -0500

    Updated to 0.88.0b0 and moved the custom components to embedded format

--- update_files.sh
+++ update_files.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-wget -O /home/homeassistant/.homeassistant/www/custom_ui/calendar-card.js https://raw.githubusercontent.com/rdehuyss/homeassistant-lovelace-google-calendar-card/master/calendar-card.js
 wget -O /home/homeassistant/.homeassistant/www/custom_ui/slider-entity-row.js https://raw.githubusercontent.com/thomasloven/lovelace-slider-entity-row/master/slider-entity-row.js
 wget -O /home/homeassistant/.homeassistant/custom_components/media_player/alexa.py https://raw.githubusercontent.com/keatontaylor/custom_components/master/media_player/alexa.py
 wget -O /home/homeassistant/.homeassistant/www/custom_ui/button-card.js https://raw.githubusercontent.com/kuuji/button-card/master/button-card.js
--- www/custom_ui/slider-entity-row.js
+++ www/custom_ui/slider-entity-row.js
@@ -157,6 +157,14 @@ class SliderEntityRow extends Polymer.Element {
     this.min = config.min || 0;
     this.max = config.max || 100;
     this.step = config.step || 5;
+
+    if(this._hass && this._config) {
+      this.stateObj = this._config.entity in this._hass.states ? this._hass.states[this._config.entity] : null;
+      if(this.stateObj) {
+        this.value = this.controller.get(this.stateObj);
+        this.displaySlider = this.controller.supported(this.stateObj);
+      }
+    }
   }
 
   statusString(stateObj) {
@@ -167,10 +175,13 @@ class SliderEntityRow extends Polymer.Element {
 
   set hass(hass) {
     this._hass = hass;
-    this.stateObj = this._config.entity in hass.states ? hass.states[this._config.entity] : null;
-    if(this.stateObj) {
-      this.value = this.controller.get(this.stateObj);
-      this.displaySlider = this.controller.supported(this.stateObj);
+
+    if(hass && this._config) {
+      this.stateObj = this._config.entity in hass.states ? hass.states[this._config.entity] : null;
+      if(this.stateObj) {
+        this.value = this.controller.get(this.stateObj);
+        this.displaySlider = this.controller.supported(this.stateObj);
+      }
     }
   }
 
