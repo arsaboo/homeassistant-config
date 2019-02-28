5b93761d14db3487d4f7468f901566d1a37a99fe
Alok <arsaboo@gmx.com>
2019-02-28 11:16:16 -0500

    Update custom cards

--- update_files.sh
+++ update_files.sh
@@ -4,6 +4,6 @@ wget -O /home/homeassistant/.homeassistant/www/custom_ui/slider-entity-row.js ht
 wget -O /home/homeassistant/.homeassistant/www/custom_ui/auto-entities.js https://raw.githubusercontent.com/thomasloven/lovelace-auto-entities/master/auto-entities.js
 wget -O /home/homeassistant/.homeassistant/www/custom_ui/card-tools.js https://raw.githubusercontent.com/thomasloven/lovelace-card-tools/master/card-tools.js
 wget -O /home/homeassistant/.homeassistant/www/custom_ui/button-card.js https://raw.githubusercontent.com/custom-cards/button-card/master/button-card.js
-wget -O /home/homeassistant/.homeassistant/www/custom_ui/layout-card.js https://github.com/thomasloven/lovelace-layout-card/blob/master/layout-card.js
+wget -O /home/homeassistant/.homeassistant/www/custom_ui/layout-card.js https://raw.githubusercontent.com/thomasloven/lovelace-layout-card/master/layout-card.js
 wget -O /home/homeassistant/.homeassistant/www/custom_ui/gap-card.js https://github.com/thomasloven/lovelace-gap-card/blob/master/gap-card.js
 sudo chown -R homeassistant:homeassistant /home/homeassistant/.homeassistant/
