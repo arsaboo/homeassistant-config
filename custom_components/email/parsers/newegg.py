02e4b00eac3c5d14bae118969d57d6b23817e902
Alok <arsaboo@gmx.com>
2019-08-19 14:23:59 -0400

    Update email sensor

--- custom_components/email/parsers/newegg.py
+++ custom_components/email/parsers/newegg.py
@@ -22,12 +22,28 @@ def parse_newegg(emails):
 
         if EMAIL_ADDRESS in email_from:
             soup = BeautifulSoup(email[EMAIL_ATTR_BODY], 'html.parser')
-            links = [link.get('href') for link in soup.find_all('a')]
-            for link in links:
-                if not link: continue
+
+            link_urls = [link.get('href') for link in soup.find_all('a')]
+            for link in link_urls:
+                if not link: 
+                    continue
                 match = re.search('TrackingNumber=(.*?)&type=0', link)
                 if match and match.group(1) not in tracking_numbers:
                     tracking_numbers.append(match.group(1))
-                
+            
+            # sometimes tracking numbers are text in a link
+            strongs = [link for link in soup.find_all('strong')]
+            for strong in strongs:
+                if not strong.get_text():
+                    continue
+                match = re.search('Tracking Number', strong.get_text())
+                if match:
+                    link_texts = [link.get_text() for link in 
+                                    strong.findChildren("a", recursive=False)]
+                    for link_text in link_texts:
+                        if not link_text:
+                            continue
+                        if link_text not in tracking_numbers:
+                            tracking_numbers.append(link_text)
+
     return tracking_numbers
-    
