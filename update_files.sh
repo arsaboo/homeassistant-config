2810f6ae6704b0a7c0788a3ec36ceeeae999fe95
Alok <arsaboo@gmx.com>
2019-08-25 19:16:23 -0400

    More HACS updates

--- update_files.sh
+++ update_files.sh
@@ -5,5 +5,4 @@ wget -O /home/homeassistant/.homeassistant/www/custom_ui/card-tools.js https://r
 wget -O /home/homeassistant/.homeassistant/www/custom_ui/layout-card.js https://raw.githubusercontent.com/thomasloven/lovelace-layout-card/master/layout-card.js
 wget -O /home/homeassistant/.homeassistant/www/custom_ui/gap-card.js https://raw.githubusercontent.com/thomasloven/lovelace-gap-card/master/gap-card.js
 wget -O /home/homeassistant/.homeassistant/www/custom_ui/card-modder.js https://raw.githubusercontent.com/thomasloven/lovelace-card-modder/master/card-modder.js
-wget -O /home/homeassistant/.homeassistant/www/custom_ui/simple-thermostat.js https://github.com/nervetattoo/simple-thermostat/releases/download/0.25.0/simple-thermostat.js
 sudo chown -R homeassistant:homeassistant /home/homeassistant/.homeassistant/
